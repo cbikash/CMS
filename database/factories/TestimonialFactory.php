8345a1b169a2e60c6fd659bd2c377d88dbbe1ea8
Bikas chaudhary <vcbikas123@gmail.com>
2021-08-20 21:08:50 +0545

    databases added

---
+++ database/factories/TestimonialFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Testimonial;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TestimonialFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Testimonial::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_08_20_144048_create_projects_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('description');
+            $table->string('image');
+            $table->string('project_start_date');
+            $table->string('project_end_date');
+            $table->string('project_status');
+            $table->string('project_type');
+            $table->string('project_url');
+            $table->string('location');
+            $table->string('project_value');
+            $table->string('project_owner');
+            $table->string('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
